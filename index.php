9af8ddefaae84fad8a689bc83131b9b7fff713cd
Lyz03 <lizoe@netcourrier.com>
2022-02-25 10:17:37 +0100

    exo 202

--- index.php
+++ index.php
@@ -17,7 +17,25 @@
  */
 
 // TODO Votre code ici.
+$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$stmt = $db->prepare("
+    SELECT article.id, article.title, article.content, categorie.name
+    FROM article
+    INNER JOIN categorie ON article.category_fk = categorie.id    
+");
+
+
+if($stmt->execute()) {
+    foreach ($stmt->fetchAll() as $value) {
+        foreach ($value as $key => $item) {
+            echo $key . ' : ' . $item . '<br>';
+        }
+        echo '<br><br>';
+    }
+}
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
@@ -25,6 +43,21 @@
 
 // TODO Votre code ici.
 
+$stmt = $db->prepare("
+    SELECT a.id, a.title, a.content, c.name
+    FROM article AS a
+    INNER JOIN categorie AS c ON a.category_fk = c.id    
+");
+
+
+if($stmt->execute()) {
+    foreach ($stmt->fetchAll() as $value) {
+        foreach ($value as $key => $item) {
+            echo $key . ' : ' . $item . '<br>';
+        }
+        echo '<br><br>';
+    }
+}
 
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
@@ -33,3 +66,19 @@
  */
 
 // TODO Votre code ici.
+
+$stmt = $db->prepare("
+    SELECT commentaire.content, utilisateur.firstName, utilisateur.lastName
+    FROM commentaire
+    INNER JOIN utilisateur ON commentaire.user_fk = utilisateur.id    
+");
+
+
+if($stmt->execute()) {
+    foreach ($stmt->fetchAll() as $value) {
+        foreach ($value as $key => $item) {
+            echo $key . ' : ' . $item . '<br>';
+        }
+        echo '<br><br>';
+    }
+}
